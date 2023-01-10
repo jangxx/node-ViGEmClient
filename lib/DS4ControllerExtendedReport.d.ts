1324f26db83e7a43443a4fc031c7d091fc8aa34c
jangxx <janscheiper95@gmail.com>
2023-01-10 22:01:58 +0100

    another small type definition fix

--- lib/DS4ControllerExtendedReport.d.ts
+++ lib/DS4ControllerExtendedReport.d.ts
@@ -10,6 +10,6 @@ export class DS4ControllerExtendedReport extends DS4ControllerReport {
 
 export class DS4TrackpadTouch {
 	getFingersDown(): number;
-	addFingerDown(x: number, y: number);
+	addFingerDown(x: number, y: number): void;
 	allFingersUp(): void;
 }
